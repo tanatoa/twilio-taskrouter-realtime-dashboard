b4e0c72b0c4ac8246ba9daff627057c75e22712e
Anas <elharda@gmail.com>
2019-10-08 09:17:15 -0500

    Update taskrouter_dashboard.js

--- static/taskrouter_dashboard.js
+++ static/taskrouter_dashboard.js
@@ -154,9 +154,9 @@ var taskrouterDashboard = new Vue({
         }
         this.totalWorkers = data['totalWorkers'];
         this.currentWorkerActivity['offlineWorkers'] = data['activityOfflineWorkers'];
-        this.currentWorkerActivity['availableWorkers'] = data['activityAvailableWorkers'] - data['assignedTasks'];
         this.currentWorkerActivity['reservedWorkers'] = data['activityReservedWorkers'];
         this.currentWorkerActivity['busyWorkers'] = data['activityBusyWorkers'];
+        this.currentWorkerActivity['availableWorkers'] = data['activityAvailableWorkers'] - data['activityReservedWorkers'];
       }
     },
     serverSideStatsInit: function() {
